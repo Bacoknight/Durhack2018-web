663c447be46d262b61e53cf242d5c4ad8f960ff9
KKT <kkt2808@gmail.com>
2018-11-17 20:07:00 +0000

    Testing Applet.

---
+++ Back End (Java)/app/src/main/java/a2018/durhack/backendjava/testingApplet.java
@@ -0,0 +1,16 @@
+package main.java.a2018.durhack.backendjava;
+
+import javax.swing.JApplet;
+
+public class testClass extends JApplet {
+
+    private static final long serialVersionUID = 1L;
+
+    public testClass() {
+        //Required constructor.
+    }
+
+    public String returnTestString(){
+        return "Test String Success";
+    }
+}
